4d3d970ceee8e7dbbc158c852a5715a263baab67
Marc Wren <things@soul-fresh.com>
2019-11-25 22:08:11 -0700

    Updates

---
+++ precache-manifest.2a7b583112d252b85d59df431d7fdee1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4123901b44c4c3fe137e3d2047000f0",
+    "url": "/guitar-practice/index.html"
+  },
+  {
+    "revision": "104958d91955826591bc",
+    "url": "/guitar-practice/static/css/4.1144119f.chunk.css"
+  },
+  {
+    "revision": "c5ced53ffa1c99a4c14c",
+    "url": "/guitar-practice/static/css/main.216f0b16.chunk.css"
+  },
+  {
+    "revision": "8f9a8b7222f2a77771ee",
+    "url": "/guitar-practice/static/js/2.838a7b4c.chunk.js"
+  },
+  {
+    "revision": "d2a4528163f4fe038289",
+    "url": "/guitar-practice/static/js/3.52ee5ebf.chunk.js"
+  },
+  {
+    "revision": "104958d91955826591bc",
+    "url": "/guitar-practice/static/js/4.3af341d1.chunk.js"
+  },
+  {
+    "revision": "c5ced53ffa1c99a4c14c",
+    "url": "/guitar-practice/static/js/main.05ef5f09.chunk.js"
+  },
+  {
+    "revision": "398fd05d4124030026c8",
+    "url": "/guitar-practice/static/js/runtime-main.f88105a7.js"
+  },
+  {
+    "revision": "ce63c0459a272d8301b186ec73db37e7",
+    "url": "/guitar-practice/static/media/check-circle.ce63c045.svg"
+  },
+  {
+    "revision": "d3a070848269031d2d69df7a697405a5",
+    "url": "/guitar-practice/static/media/circle.d3a07084.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78aa21eb15e7afb8fcd4590d3d9be3f1.js"
+  "/guitar-practice/precache-manifest.2a7b583112d252b85d59df431d7fdee1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/guitar-practice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
