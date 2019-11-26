feb62ab0e0c539d0fe2ebba1e1adf291b5b2a7b4
Marc Wren <things@soul-fresh.com>
2019-11-25 21:44:53 -0700

    Updated App

---
+++ precache-manifest.78aa21eb15e7afb8fcd4590d3d9be3f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e2a2290c14b4c67fb0f13c3cd36c80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "104958d91955826591bc",
+    "url": "/static/css/4.1144119f.chunk.css"
+  },
+  {
+    "revision": "90ab23ea080fdf14db0d",
+    "url": "/static/css/main.216f0b16.chunk.css"
+  },
+  {
+    "revision": "8f9a8b7222f2a77771ee",
+    "url": "/static/js/2.838a7b4c.chunk.js"
+  },
+  {
+    "revision": "d2a4528163f4fe038289",
+    "url": "/static/js/3.52ee5ebf.chunk.js"
+  },
+  {
+    "revision": "104958d91955826591bc",
+    "url": "/static/js/4.3af341d1.chunk.js"
+  },
+  {
+    "revision": "90ab23ea080fdf14db0d",
+    "url": "/static/js/main.929e6ee0.chunk.js"
+  },
+  {
+    "revision": "f6e36691cd7ebfe536de",
+    "url": "/static/js/runtime-main.a16a2e96.js"
+  },
+  {
+    "revision": "ce63c0459a272d8301b186ec73db37e7",
+    "url": "/static/media/check-circle.ce63c045.svg"
+  },
+  {
+    "revision": "d3a070848269031d2d69df7a697405a5",
+    "url": "/static/media/circle.d3a07084.svg"
+  }
+]);
